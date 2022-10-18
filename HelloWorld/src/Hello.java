279f63f7080a3d92f478989edc52fd3e008e7b83
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 10:12:51 -0400

    Printing Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int thisNumber = 5;
+
+        System.out.println(thisNumber);
     }
 }
