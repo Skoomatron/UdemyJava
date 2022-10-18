44665673391aaffd39b8b08c2ba8357dfd0f3953
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 10:46:57 -0400

    Exercise 26 Complete

--- PrimitiveTypesNumbers/src/Main.java
+++ PrimitiveTypesNumbers/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
+        byte byteMin = Byte.MIN_VALUE;
+        byte byteMax = Byte.MAX_VALUE;
+
+        System.out.println("Byte Min = " + byteMin);
+        System.out.println("Byte Max = " + byteMax);
+
+        short shortMin = Short.MIN_VALUE;
+        short shortMax = Short.MAX_VALUE;
+
+        System.out.println("Short min = " + shortMin);
+        System.out.println("Short max = " + shortMax);
+
         int intMinValue = Integer.MIN_VALUE;
         int intMaxValue = Integer.MAX_VALUE;
 
@@ -11,5 +24,10 @@ public class Main {
             // computes to negative because it cycles around the maximum value to the first min value
             // the opposite is true on min value but referred to as underflow
 
+        long longMin = Long.MIN_VALUE;
+        long longMax = Long.MAX_VALUE;
+
+        System.out.println("Long min = " + longMin);
+        System.out.println("Long max = " + longMax);
     }
 }
