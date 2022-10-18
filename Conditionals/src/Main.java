56627b1ff2fe747e67434831188f31c49b528352
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 13:06:18 -0400

    Exercise 36 Complete: Conditionals

--- Conditionals/src/Main.java
+++ Conditionals/src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+
         boolean boolOne = false;
-        if (boolOne == false) {
+        int score = 58;
+
+        if (!boolOne) {
             System.out.println("This logic block is evaluated to false.");
-            boolOne = true;
+
         } else {
             System.out.println("This logic block is evaluated to true.");
         }
 
-        if (boolOne == true) {
-            System.out.println("Here I am chaining conditionals while this video puts me to sleep.");
+        if (score > 60) {
+            System.out.println("You scored over 50");
+        } else if (score == 58) {
+            System.out.println("wow");
+        } else {
+            System.out.println("Big fails");
         }
     }
 }
