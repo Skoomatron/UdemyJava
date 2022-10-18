6328125fae3b0c773b6540dbf1b1df56a382d94e
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 12:46:52 -0400

    Exercise 33 Complete: Operators

---
+++ Operators/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = 1 + 2;
+
+        System.out.println("One plus two equals " + resultOne);
+
+        int resultTwo = resultOne - 1;
+
+        System.out.println("Result two is equal to " + resultTwo);
+
+        resultOne = resultOne * 10;
+
+        System.out.println("Result one is now equal to " + resultOne);
+
+        resultOne = resultOne / 5;
+
+        System.out.println("Result one is now eequal to " + resultOne);
+
+        resultOne = resultOne % 4;
+
+        System.out.println("The remainder of result one is " + resultOne);
+    }
+}
