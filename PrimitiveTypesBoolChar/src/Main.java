17aba4f1e4d8430f83f009228021f536baa58829
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 12:07:42 -0400

    Exercise 31 Complete: Booleans & Chars

---
+++ PrimitiveTypesBoolChar/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char charOne = 'f'; // only stores one character
+        char uniCode = '\u00D4'; // \u must precede all unicode characters
+
+        System.out.println("Char one = " + charOne);
+        System.out.println("Char two = " + uniCode);
+
+
+        boolean boolOne = true;
+        boolean boolTwo = false;
+
+        System.out.println("Boolean one = " + boolOne);
+        System.out.println("Boolean two = " + boolTwo);
+    }
+}
