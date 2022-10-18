b90aead220252e2801a9da17d3110d095b998c49
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 15:10:29 -0400

    Leap Year Calculator Complete

---
+++ LeapYearCalculator/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean result = isLeapYear(1600);
+        System.out.println(result);
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
