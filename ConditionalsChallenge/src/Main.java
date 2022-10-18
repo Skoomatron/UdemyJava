f87f5d143ff7155a48565938ad6678bfbcd6c326
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 13:24:40 -0400

    Conditional Challenge Complete

---
+++ ConditionalsChallenge/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        double doubleOne = 20.00;
+        double doubleTwo = 80.00;
+
+        double result = (doubleTwo + doubleOne) * 100;
+
+        double remainder = result % 40.00;
+
+        boolean check = remainder == 0 ? true : false;
+
+        System.out.println("Check is equal to " + check);
+
+        if (!check) {
+            System.out.println("There was some junk left.");
+        }
+    }
+}
