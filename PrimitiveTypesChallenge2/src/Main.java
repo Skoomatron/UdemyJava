c07c0b309f3145bcbad630ef1fc5265356628385
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-18 11:19:51 -0400

    Primitive Types Challenge 2 Completed - Doubles and Floats

---
+++ PrimitiveTypesChallenge2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double pounds = 10;
+        double factor = 0.45259237d;
+
+        System.out.println("The number of kilograms in 10 pounds = " + (pounds * factor));
+    }
+}
