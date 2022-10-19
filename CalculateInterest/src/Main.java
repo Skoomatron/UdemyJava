8135e28de8b8b9d11a932f1a21544b15e0094725
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 10:00:26 -0400

    Calculate Interest Complete

---
+++ CalculateInterest/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 9; i++) {
+            double total = calculateInterest(10000, i);
+            System.out.println("Monies are " + total);
+        }
+    }
+
+    public static double calculateInterest(double amount, double rate) {
+        return (amount * (rate / 100));
+    }
+}
