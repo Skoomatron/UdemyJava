3cc7e94924d4e7fc1ae7133b269fd67fd6d94b8c
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 08:44:01 -0400

    Exercise 62 Completed: Switch Statements

---
+++ Switches/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int testValue = 1;
+
+        switch(testValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            default:
+                System.out.println("No value found");
+                break;
+        }
+    }
+}
