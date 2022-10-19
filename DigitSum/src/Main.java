eedb49403ae1cc0947b8952d3587ca9e53c5d4f4
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 10:43:19 -0400

    Digit Sum Completed

---
+++ DigitSum/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(1223));
+    }
+
+    public static int sumDigits(int num) {
+        int total = 0;
+        int current = num;
+        int remainder = 0;
+
+        do {
+            // 122 12
+            remainder = current % 10; // 2 2
+            total += remainder; // 2 4
+            current = (current - remainder) / 10; // 12 1
+            System.out.println(total);
+        } while (current > 10);
+
+        total += current;
+        return total;
+    }
+}
