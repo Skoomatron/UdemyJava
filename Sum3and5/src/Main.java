ffc62763eefd96cc86b3a52610fc1dd00e471add
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 10:14:41 -0400

    3 and 5 Completed

--- Sum3and5/src/Main.java
+++ Sum3and5/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         int counter = 0;
 
         for (int i = 1; i <= 1000; i++) {
-            if (counter == 5) {
-                System.out.println("Total of numbers is " + total);
-                break;
-            }
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 total += i;
                 counter++;
                 System.out.println("This number works " + i);
+            }
+            if (counter == 5) {
+                System.out.println("Total of numbers is " + total);
+                break;
             }
         }
     }
