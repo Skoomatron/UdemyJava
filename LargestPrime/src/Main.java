f8248fef731ce0eebf45ba772a21ebb64b4b2c87
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 14:39:22 -0400

    Largest Prime Complete

---
+++ LargestPrime/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(217));
+    }
+
+    public static int getLargestPrime(int number){
+        if(number <= 1){
+            return -1;
+        }
+        int i = 2;
+        int j = number;
+        int k = 0;
+        while(i <= number / 2){
+            while (j % i == 0){
+                j /=i;
+                k = i;
+            }
+            i++;
+        }
+        if(k != 0){
+            return k;
+        }
+        else{
+            return number;
+        }
+    }
+}
