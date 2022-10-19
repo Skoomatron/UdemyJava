c81ddc1078b87b1be5e6845b5b2f5ae15053737e
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-19 11:35:39 -0400

    Shared Digit Complete

--- SharedDigit/src/Main.java
+++ SharedDigit/src/Main.java
@@ -3,10 +3,7 @@ public class Main {
         System.out.println(hasSharedDigit(12, 23));
         System.out.println(hasSharedDigit(9, 99));
         System.out.println(hasSharedDigit(15, 55));
-
-
     }
-
     public static boolean hasSharedDigit(int a, int b) {
         if (a < 10 || a > 99 || b < 10 || b > 99) {
             return false;
