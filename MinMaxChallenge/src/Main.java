139070df0c5e29aa5806ce58f9deadc83f5b2f21
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-20 08:05:23 -0400

    User Min/Max Completed

---
+++ MinMaxChallenge/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int min = 0;
+        int max = 0;
+
+        do {
+            System.out.println("Enter A Number: ");
+            boolean check = scanner.hasNextInt();
+            if (check) {
+                int current = scanner.nextInt();
+                scanner.nextLine();
+                if (current > max) {
+                    max = current;
+                }
+                if (current < min) {
+                    min = current;
+                }
+            } else {
+                System.out.println("Invalid Entry!");
+                break;
+            }
+
+            System.out.println("Current minimum number is: " + min);
+            System.out.println("Current maximum number is: " + max);
+        } while (true);
+
+        scanner.close();
+    }
+}
